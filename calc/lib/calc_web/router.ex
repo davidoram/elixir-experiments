3802ad93337524039e285d7432e9c3d6d16d76ac
David Oram <david.oram99@gmail.com>
2021-10-06 07:51:35 +1300

    Log response

--- calc/lib/calc_web/router.ex
+++ calc/lib/calc_web/router.ex
@@ -1,8 +1,18 @@
 defmodule CalcWeb.Router do
+  require Logger
   use Plug.ErrorHandler
   use CalcWeb, :router
 
+  def my_logger(conn, _opts) do
+    conn = Plug.Conn.register_before_send(conn, fn conn ->
+      Logger.info("Sent a #{conn.status} response")
+      conn
+    end)
+    conn
+  end
+
   pipeline :api do
+    plug :my_logger # custom plug
     plug :accepts, ["json"]
   end
 
@@ -15,7 +25,6 @@ defmodule CalcWeb.Router do
   # See https://www.amberbit.com/blog/2018/7/24/when-web-requests-fail-in-elixir-and-phoenix/
   @impl Plug.ErrorHandler
   def handle_errors(conn, %{kind: _kind, reason: _reason, stack: _stack}) do
-    logger
     data = %{
       kind:           "Errors",
       id:             "{uuid}",
@@ -25,4 +34,5 @@ defmodule CalcWeb.Router do
     response = Phoenix.json_library().encode_to_iodata!(data)
     send_resp(conn, 500, response)
   end
+
 end
